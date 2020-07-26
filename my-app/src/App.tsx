67e1a58bf11c96a7c43f3167c4562397e8d54257
john <jbankc@gmail.com>
2020-07-26 19:21:43 +1200

    update text and logo

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,24 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src="https://openmoji.org/data/color/svg/1F532.svg" className="App-logo" alt="logo" />
+                <p>
+                    MSA 2020
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+                <a
+                    className="App-link"
+                    href="https://aka.ms/nzmsawebsite"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    MSA website
         </a>
-      </header>
-    </div>
-  );
+            </header>
+        </div>
+    );
 }
 
 export default App;
